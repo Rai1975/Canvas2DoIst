8265d0b9f4fd538c057dc3cfd35947db4088af84
Raihan Rafeek <raihanrafeek@gmail.com>
2024-10-23 14:20:53 -0400

    Modifying automation scripts and adding bash script.

---
+++ automation_scripts/bash_automation.sh
@@ -0,0 +1,25 @@
+function synclist {
+    # Start Flask app in the background
+    nohup python3 /path/to/your/app.py > flask_output.log 2>&1 &
+    echo "Starting Script..."
+
+    flask_pid=$!
+
+    sleep 5
+
+    echo "Syncing Assignments..."
+    response=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:5000/sync-assignments)
+
+    # Check if the response code is 200 (OK)
+    if [ "$response" -eq 200 ]; then
+        echo "Sync successful..."
+    else
+        echo "Error: Sync failed with status code $response."
+    fi
+
+    # Kill the Flask app
+    kill $flask_pid
+
+    # Print Done when finished
+    echo "Done"
+}
